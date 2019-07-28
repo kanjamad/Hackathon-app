16ddd80d6baf10d953be9d6eb6cea1586ea55db3
kanjamad bosel <kanjamadpor@gmail.com>
2019-07-28 09:19:48 -0700

    add toggle function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log("hi");
+
+function classToggle() {
+    this.classList.toggle('like1');
+    this.classList.toggle('like2');
+}
+document.querySelector('#love').addEventListener('click', classToggle);
+
+function classToggle() {
+    this.classList.toggle('like1');
+    this.classList.toggle('like2');
+}
+document.querySelector('#love2').addEventListener('click', classToggle);
